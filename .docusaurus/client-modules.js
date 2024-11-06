5b157dfd23cc25f61f478113517e2633e4d754bf
xyh2023 <xalwayswill@outlook.com>
2024-11-06 21:56:31 +0800

    Update cdc

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("/home/xyh/Work/xalwayswill.github.io/node_modules/infima/dist/css/default/default.css"),
-  require("/home/xyh/Work/xalwayswill.github.io/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/home/xyh/Work/xalwayswill.github.io/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/home/xyh/Work/xalwayswill.github.io/src/css/custom.css"),
-];
